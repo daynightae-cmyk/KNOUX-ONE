720969e017402008a4a796aff588a0770bcd8317
DAY NIGHT DELIVERY SERVICES <daynight.ae@gmail.com>
2026-07-24 20:12:56 +0400

    fix(honesty): present Module 15 as planned workspace

--- src/components/views/DeveloperSuiteView.tsx
+++ src/components/views/DeveloperSuiteView.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
-import { DeveloperStudio } from '../../features/developer/DeveloperStudio';
+import { UniversalServiceWorkspace } from '../common/UniversalServiceWorkspace';
+import { MODULES_CATALOG } from '../../data/capabilitiesCatalog';
 
 export const DeveloperSuiteView: React.FC = () => {
-  return <DeveloperStudio />;
+  const module = MODULES_CATALOG.find(item => item.id === 'm15');
+  if (!module) return null;
+  return (
+    <UniversalServiceWorkspace
+      moduleNumber={module.number}
+      moduleNameEn={module.nameEn}
+      moduleNameAr={module.nameAr}
+      descriptionEn={module.descriptionEn}
+      descriptionAr={module.descriptionAr}
+      capabilities={module.services}
+    />
+  );
 };
