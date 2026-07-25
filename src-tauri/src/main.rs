202447ec1e85a6c3637e16c6fc1f1be7d588db55
DAY NIGHT DELIVERY SERVICES <daynight.ae@gmail.com>
2026-07-25 06:24:53 +0400

    fix(m15): allow explicit operation-result builder arguments

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,7 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-#![allow(clippy::unnecessary_sort_by)]
+#![allow(clippy::unnecessary_sort_by, clippy::too_many_arguments)]
 
 mod contracts;
 mod developer;
