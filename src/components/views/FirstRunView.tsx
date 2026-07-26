4d9eefba850c3268e86121dc0f0ec5023bf84261
DAY NIGHT DELIVERY SERVICES <daynight.ae@gmail.com>
2026-07-26 23:15:59 +0400

    feat(completion14): replace generic Module 01 page with real setup workspace

--- src/components/views/FirstRunView.tsx
+++ src/components/views/FirstRunView.tsx
@@ -1,19 +1,4 @@
 import React from 'react';
-import { MODULES_CATALOG } from '../../data/capabilitiesCatalog';
-import { UniversalServiceWorkspace } from '../common/UniversalServiceWorkspace';
+import { WindowsSetupWorkspace } from '../../features/setup/WindowsSetupWorkspace';
 
-export const FirstRunView: React.FC = () => {
-  const moduleData = MODULES_CATALOG.find(m => m.id === 'm01');
-  if (!moduleData) return null;
-
-  return (
-    <UniversalServiceWorkspace
-      moduleNumber={1}
-      moduleNameEn={moduleData.nameEn}
-      moduleNameAr={moduleData.nameAr}
-      descriptionEn={moduleData.descriptionEn}
-      descriptionAr={moduleData.descriptionAr}
-      capabilities={moduleData.services}
-    />
-  );
-};
+export const FirstRunView: React.FC = () => <WindowsSetupWorkspace />;
