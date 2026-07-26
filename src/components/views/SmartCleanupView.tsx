e10f98cd65b9809c99b6d5f1c992338378f19584
DAY NIGHT DELIVERY SERVICES <daynight.ae@gmail.com>
2026-07-26 23:14:11 +0400

    feat(completion14): surface reversible download quarantine in cleanup view

--- src/components/views/SmartCleanupView.tsx
+++ src/components/views/SmartCleanupView.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { DownloadQuarantinePanel } from '../../features/cleanup/DownloadQuarantinePanel';
 import { SmartCleanupWorkspace } from '../../features/cleanup/SmartCleanupWorkspace';
 
-export const SmartCleanupView: React.FC = () => <SmartCleanupWorkspace />;
+export const SmartCleanupView: React.FC = () => (
+  <div className="space-y-6">
+    <SmartCleanupWorkspace />
+    <DownloadQuarantinePanel />
+  </div>
+);
