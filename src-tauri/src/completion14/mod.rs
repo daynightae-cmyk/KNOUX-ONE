cf8e52929d884fda660cee1966a73e198667374b
DAY NIGHT DELIVERY SERVICES <daynight.ae@gmail.com>
2026-07-26 23:32:12 +0400

    fix(completion14): keep Tauri commands in their defining modules

--- src-tauri/src/completion14/mod.rs
+++ src-tauri/src/completion14/mod.rs
@@ -2,23 +2,3 @@ pub mod m01;
 pub mod m02;
 pub mod m03;
 pub mod m04;
-
-pub use m01::{
-    m01_system_discover_complete, m01_winget_install_queued, m01_winget_queue_list,
-    m01_winget_queue_resume,
-};
-pub use m02::{
-    m02_cleanup_cancel_complete, m02_cleanup_execute_complete, m02_cleanup_history_complete,
-    m02_cleanup_scan_complete, m02_download_quarantine_list, m02_download_quarantine_restore,
-    m02_scan_application_logs_complete, m02_scan_crash_dumps_complete,
-    m02_scan_old_downloads_complete, m02_scan_windows_temp_complete,
-};
-pub use m03::{
-    m03_scan_archives_complete, m03_scan_audio_complete, m03_scan_images_complete,
-    m03_scan_videos_complete,
-};
-pub use m04::{
-    m04_appdata_complete, m04_downloads_complete, m04_external_drives_complete,
-    m04_old_files_complete, m04_report_export_complete, m04_scan_cancel_complete,
-    m04_space_check_complete, m04_storage_scan_complete, start_persisted_monitor,
-};
