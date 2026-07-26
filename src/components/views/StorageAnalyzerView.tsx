ddf7f8f69a03437a78771e519a65d723da283c43
DAY NIGHT DELIVERY SERVICES <daynight.ae@gmail.com>
2026-07-26 18:19:44 +0400

    feat(m04): route storage analysis to dedicated workspace

--- src/components/views/StorageAnalyzerView.tsx
+++ src/components/views/StorageAnalyzerView.tsx
@@ -1,19 +1,4 @@
 import React from 'react';
-import { MODULES_CATALOG } from '../../data/capabilitiesCatalog';
-import { UniversalServiceWorkspace } from '../common/UniversalServiceWorkspace';
+import { StorageAnalyzerWorkspace } from '../../features/storage/StorageAnalyzerWorkspace';
 
-export const StorageAnalyzerView: React.FC = () => {
-  const moduleData = MODULES_CATALOG.find(m => m.id === 'm04');
-  if (!moduleData) return null;
-
-  return (
-    <UniversalServiceWorkspace
-      moduleNumber={4}
-      moduleNameEn={moduleData.nameEn}
-      moduleNameAr={moduleData.nameAr}
-      descriptionEn={moduleData.descriptionEn}
-      descriptionAr={moduleData.descriptionAr}
-      capabilities={moduleData.services}
-    />
-  );
-};
+export const StorageAnalyzerView: React.FC = () => <StorageAnalyzerWorkspace />;
